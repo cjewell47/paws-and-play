e8493180b958ad6b5c226e6f37a76c8aa3bd0c1b
hassanisk <hiskaf1@gmail.com>
2017-05-15 10:30:00 +0100

    front end registration

---
+++ src/js/controllers/register.controller.js
@@ -0,0 +1,24 @@
+angular
+.module('DogApp')
+.controller('RegisterCtrl', RegisterCtrl);
+
+RegisterCtrl.$inject = ['User', 'CurrentUserService', '$state'];
+function RegisterCtrl(User, CurrentUserService, $state){
+  const vm = this;
+
+  vm.register = register;
+
+  function register() {
+    User
+      .register(vm.user)
+      .$promise
+      .then(data => {
+        console.log(data);
+        CurrentUserService.getUser();
+        $state.go('usersIndex');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+}
---
+++ src/js/factories/user.factory.js
@@ -0,0 +1,11 @@
+angular
+.module('DogApp')
+.factory('User', userFactory);
+
+userFactory.$inject = ['API', '$resource'];
+function userFactory(API, $resource){
+  return $resource(`${API}/users/:id`, { id: '@_id'}, {
+    'register': { method: 'POST', url: `${API}/register` },
+    'login': { method: 'POST', url: `${API}/login` }
+  });
+}
