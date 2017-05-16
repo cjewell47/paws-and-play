34bf13c445b5a317e2a221115495d5de4c060588
cjewell47 <charles.jewell47@gmail.com>
2017-05-16 18:04:42 +0100

    filtering beginning to work, and walk model made ..sort of

--- src/js/controllers/dogs-index.controller.js
+++ src/js/controllers/dogs-index.controller.js
@@ -2,13 +2,25 @@ angular
 .module('DogApp')
 .controller('DogIndexCtrl', DogIndexCtrl);
 
-DogIndexCtrl.$inject = ['Dog'];
-
-function DogIndexCtrl (Dog) {
+DogIndexCtrl.$inject = ['Dog', 'filterFilter', '$scope'];
+function DogIndexCtrl (Dog, filterFilter, $scope) {
   const vm = this;
-  dogsIndex();
-  function dogsIndex() {
-    vm.dogs = Dog.query();
+  vm.dogs = Dog.query();
+  // dogsIndex();
 
+  // function dogsIndex() {
+    // vm.dogs = Dog.query();
+
+  function filterDogs() {
+    //const params = { name: vm.q };
+    //if(vm.useBreed) params.breed = vm.breed;
+    //if(vm.useSize) params.size = vm.size;
+
+    vm.filtered = filterFilter(vm.dogs, vm.q);
   }
+
+  $scope.$watch(() => vm.q, filterDogs);
+
+  vm.filterDogs = filterDogs;
+  // }
 }
