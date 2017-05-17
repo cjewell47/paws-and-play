a51d2eca7eac2a9c35f71ddacb04a26bb8dca6aa
hassanisk <hiskaf1@gmail.com>
2017-05-17 13:23:41 +0100

    embeddded

--- models/walk.js
+++ models/walk.js
@@ -1,26 +1,26 @@
-const mongoose  = require('mongoose');
-
-const walkSchema = new mongoose.Schema({
-  owner: { type: mongoose.Schema.ObjectId, ref: 'User' },
-  dog: { type: mongoose.Schema.ObjectId, ref: 'Dog' },
-  available: [
-    {
-      type: Date
-    }
-  ],
-  pending: [
-    {
-      date: { type: Date },
-      walker: { type: mongoose.Schema.ObjectId, ref: 'User' }
-    }
-  ],
-  accepted: [
-    {
-      date: { type: Date },
-      walker: { type: mongoose.Schema.ObjectId, ref: 'User' }
-    }
-  ]
-});
-
-
-module.exports = mongoose.model('Walk', walkSchema);
+// const mongoose  = require('mongoose');
+//
+// const walkSchema = new mongoose.Schema({
+//   owner: { type: mongoose.Schema.ObjectId, ref: 'User' },
+//   dog: { type: mongoose.Schema.ObjectId, ref: 'Dog' },
+//   available: [
+//     {
+//       type: Date
+//     }
+//   ],
+//   pending: [
+//     {
+//       date: { type: Date },
+//       walker: { type: mongoose.Schema.ObjectId, ref: 'User' }
+//     }
+//   ],
+//   accepted: [
+//     {
+//       date: { type: Date },
+//       walker: { type: mongoose.Schema.ObjectId, ref: 'User' }
+//     }
+//   ]
+// });
+//
+//
+// module.exports = mongoose.model('Walk', walkSchema);
