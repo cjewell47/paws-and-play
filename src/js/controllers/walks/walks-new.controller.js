eb87b68a503ee8a940f21b433992dc31aaf8edc7
cjewell47 <charles.jewell47@gmail.com>
2017-05-18 10:51:17 +0100

    redirects

--- src/js/controllers/walks/walks-new.controller.js
+++ src/js/controllers/walks/walks-new.controller.js
@@ -2,9 +2,9 @@ angular
 .module('DogApp')
 .controller('WalkNewCtrl', WalkNewCtrl);
 
-WalkNewCtrl.$inject = ['Dog', '$state','TokenService', '$stateParams'];
+WalkNewCtrl.$inject = ['Dog', '$state','TokenService', '$stateParams', '$location'];
 
-function WalkNewCtrl (Dog, $state, TokenService, $stateParams) {
+function WalkNewCtrl (Dog, $state, TokenService, $stateParams, $location) {
   const vm = this;
   vm.create = walkCreate;
   function walkCreate(){
@@ -13,8 +13,10 @@ function WalkNewCtrl (Dog, $state, TokenService, $stateParams) {
     .addWalk($stateParams, {date: vm.walk})
     .$promise
     .then(dog => {
-      console.log(dog);
-      $state.go('usersShow',{ id: TokenService.decodeToken().id });
+      console.log('THIS IS DOG---', dog);
+      // $state.go('usersShow',{ id: TokenService.decodeToken().id });
+      // $location.path(`/users/${dog.owner}`);
+      // $state.go('usersShow', { id: dog.owner });
     })
     .catch(err => {
       console.log(err);
