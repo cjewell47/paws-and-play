4b9a92f6597c6c9c724ddda2e3618915973cd134
cjewell47 <charles.jewell47@gmail.com>
2017-05-19 11:15:56 +0100

    seeds for deployment

--- db/seeds.js
+++ db/seeds.js
@@ -1,7 +1,7 @@
 const env        = require('../config/env');
 const mongoose   = require('mongoose');
 mongoose.Promise = require('bluebird');
-process.env.NODE_ENV = 'development';
+
 mongoose.connect(env.db[process.env.NODE_ENV]);
 
 const User = require('../models/user');
