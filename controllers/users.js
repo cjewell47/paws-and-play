1588a8ab48018e4496f3f4027d6e17a85920981e
cjewell47 <charles.jewell47@gmail.com>
2017-05-21 18:18:36 +0100

    walk confirmations and messaging fixed

--- controllers/users.js
+++ controllers/users.js
@@ -73,7 +73,7 @@ function walkConfirm(req, res, next) {
 
   Promise.all([walker, dog])
   .then(values => {
-    const walker         = values[0];
+    const walker        = values[0];
     const dog           = values[1];
     const requestDate   = new Date(req.body.walk.date);
     const indexToRemove = dog.walks.findIndex(walk => {
